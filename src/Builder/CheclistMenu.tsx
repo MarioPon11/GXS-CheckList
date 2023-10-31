5bd6bbbf6dcab07be775ad38ec10430ddb1f91f8
Mario Pon <gxs.mpon@gmail.com>
2023-10-31 16:53:12 -0600

    Dinamically populate settings

--- src/Builder/CheclistMenu.tsx
+++ src/Builder/CheclistMenu.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
 import BulletTextArea from "./attoms/CheckTextArea";
 import InputField from "./attoms/tabName";
 
@@ -7,6 +7,27 @@ interface buttonProps {
 }
 
 const AppMenu: React.FC<buttonProps> = ({ buttonF }) => {
+    const [tabs, setTabs] = useState<any[]>([]);
+    const [emails, setEmails] = useState<any[]>([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await window.api.invoke('get-data', 'Account Name');
+            setTabs(response);
+
+            const emailResponse = window.api.invoke('get-emails', '');
+            setEmails(emailResponse); // Default to the first tab's values
+        };
+        fetchData();
+    }, []);
+
+    function handleTabClick(tabName: string) {
+        const selectedTab = tabs.find((tab) => tab.name === tabName);
+        if (selectedTab) {
+            console.log('Tab close was clicked');
+        }
+    }
+
     function ButtonFunction() {
         console.log('Button was clicked');
         buttonF();
@@ -24,14 +45,18 @@ const AppMenu: React.FC<buttonProps> = ({ buttonF }) => {
                 <h2>Settings:</h2>
                 <div className="tabs-info">
                     <div className="tabs">
-                        <div className="tab">
-                            <div className="tab-control">
-                                <InputField existingData='Tab Name' />
-                                <span className='tab-control-span'></span>
-                                <button className="remove-tab"><i className="bx bx-x"></i></button>
-                            </div>
-                            <BulletTextArea existingData={['Random', 'Data', 'Here']} />
-                        </div>
+                        {
+                            tabs.map((tab, index) => (
+                                <div className="tab">
+                                    <div className="tab-control">
+                                        <InputField existingData={tab.name} />
+                                        <span className='tab-control-span'></span>
+                                        <button className="remove-tab" onClick={handleTabClick(tab.name)}><i className="bx bx-x"></i></button>
+                                    </div>
+                                    <BulletTextArea existingData={tab.values} />
+                                </div>
+                            ))
+                        }
                         <button className="add-tab">Add New<i className="bx bx-plus"></i></button>
                     </div>
                 </div>
